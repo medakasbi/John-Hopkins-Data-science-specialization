814effd9a6a75fd7df1f40e6bc13a5aec687453f
Mohamed Akasbi <medakasbi@gmail.com>
2017-08-29 08:23:23 +0100

    Add files via upload

---
+++ best.R
@@ -0,0 +1,28 @@
+
+best <- function(state, outcome){
+    data = read.csv("outcome-of-care-measures.csv", na.strings = "Not Available", stringsAsFactors = FALSE)
+    
+    
+    if(!any(is.element(unique(my_data[["state"]]), state))){
+        return("invalid state")
+    }
+    
+    if(outcome == "heart attack"){
+        out = 11
+    }else if(outcome == "heart failure"){
+        out = 17
+    }else if (outcome == "pneumonia"){
+        out = 23
+    }else{
+        return("Invalid outcome")}
+    
+    my_data <- data[c(2, 7, out)]
+    names(my_data) = c("hospital", "state", outcome)
+    #my_data = na.omit(my_data)
+    t = tapply(my_data[[outcome]], my_data$state, min, na.rm = TRUE, simplify = FALSE)
+    t[state]
+    best_Hs = my_data$hospital[which(my_data[[outcome]] == t[state] & my_data$state == state)]
+    sort(best_Hs)[1]
+    #my_data
+    
+}
---
+++ rankhospital.R
@@ -0,0 +1,43 @@
+
+
+rankhospital <- function(state, outcome, num){
+    data = read.csv("outcome-of-care-measures.csv", na.strings = "Not Available", stringsAsFactors = FALSE)
+    
+    # Check if state is valid
+    if(!any(is.element(unique(data[["State"]]), state))){
+        print("invalid state")
+    }
+    
+    # Check if outcome is valid
+    if(outcome == "heart attack"){
+        out = 11
+    }else if(outcome == "heart failure"){
+        out = 17
+    }else if (outcome == "pneumonia"){
+        out = 23
+    }else{
+        print("Invalid outcome")
+    }
+    # subset initial data 
+    my_data <- data[c(2, 7, out)]
+    # rename columns 
+    names(my_data) = c("hospital", "state", outcome)
+    # reemove NA values
+    my_data = na.omit(my_data)
+    
+    ## Order by state then outcome then hospital name
+    my_data = my_data[order( my_data[, 2], my_data[, 3], my_data[, 1]),]
+    #Split by state
+    new_data = my_data[which(my_data[,2] == state),]
+    # get the result
+    if(num == "best"){
+        num = 1
+    }else if(num == "worst"){
+        num = nrow(new_data)
+    }else{num = num}
+    
+    new_data[num, 1]
+    
+    
+    
+}
